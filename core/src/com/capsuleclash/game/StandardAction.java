67aaa6e5c22a2c1a07eca633f4a6a059a5a224f3
Jamie Corr <jamiecorr4@gmail.com>
2016-11-03 20:23:12 -0700

    Trying to configure with Junit, test for figure class compareTo()

--- core/src/com/capsuleclash/game/StandardAction.java
+++ core/src/com/capsuleclash/game/StandardAction.java
@@ -2,6 +2,11 @@ package com.capsuleclash.game;
 
 public class StandardAction implements Action {
 
+  //for testing Figure
+  private StandardAction() {
+    
+  }
+  
 	@Override
 	public int calculate() {
 		return 0;
---
+++ core/test/com/capsuleclash/game/TestFigure.java
@@ -0,0 +1,22 @@
+package com.capsuleclash.game;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+public class TestFigure {
+  private Figure figure;
+  
+  @Test
+  public void testCompareTo() {
+     figure = new Figure(2, 3, 4, 5, 6, 7, 1, "figOne", new StandardAction()));
+     figure2 = new Figure(2, 3, 4, 5, 6, 7, 1, "figTwo", new StandardAction()));
+     figure3 = new Figure(7, 6, 5, 4, 3, 2, 11, "figThree", new StandardAction()));
+
+     assertTrue(0 == figure2.compareTo(figure1));
+     assertTrue(0 > figure2.compareTo(figure3));
+
+  }
+
+}
